969774c480efcb960955397465c93d009afc05f5
Andrew Yang <andy.tubeee@gmail.com>
2023-11-18 17:13:40 -0500

    restaurant shitmf finally working

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
@@ -15,4 +16,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
