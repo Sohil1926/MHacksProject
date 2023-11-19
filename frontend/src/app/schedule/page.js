789fabf7b8b4dfd8a139343a259bcaa379ca4650
Andrew Yang <andy.tubeee@gmail.com>
2023-11-19 11:46:33 -0500

    added padding

--- frontend/src/app/schedule/page.js
+++ frontend/src/app/schedule/page.js
@@ -67,7 +67,7 @@ export default function Page() {
     <>
       <Navbar />
       <div className='mt-6 w-full'>
-        <h3 className='text-2xl mt-11 text-black text-left mb-6 font-bold font-poppins'>
+        <h3 className='text-2xl mt-11 py-5 text-black text-left mb-6 font-bold font-poppins'>
           Schedule
         </h3>
         <EventSchedule schedule={scheduleData} />
