ac802d41496635ddff4e4ec2e6fd8a1234c5606a
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2023-11-19 00:27:03 -0500

    change order

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -29,6 +29,10 @@ export default function Home() {
           <li class="me-2">
           <a href="#" class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Activity Quotes</a>
           </li>
+          <li class="me-2">
+              <a href="#" class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Scheduling</a>
+          </li>
+         
       </ul>
       <div className="mt-6 w-full md:w-3/4 lg:w-1/2 xl:w-1/3 2xl:w-1/4">
     <BudgetChart />
